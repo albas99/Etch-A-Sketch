bc4f760948269da92eab513102939cdbbfa2bf20
Faisal Albasu <albasfaisal@yandex.ru>
2021-06-19 12:06:49 +0400

    Added grid size button

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let divsContainer = document.querySelector('.divs-container');
+let resestButton = document.querySelector('.reset-button');
+let selectGridButton = document.querySelector('.select-grid');
+let removeBordersButton = document.querySelector('.remove-borders');
+window.addEventListener('load', createDivGrid(16, 16));
+
 
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,7 +18,41 @@ function createDivGrid(rows, columns) {
         let divs = document.createElement('div');
         divs.setAttribute('class', 'divs');
         divsContainer.appendChild(divs);
+
+        divs.addEventListener('mouseover', function () {
+            divs.style.backgroundColor =
+                "rgb(" +
+                random(0, 255) +
+                "," +
+                random(0, 255) +
+                "," +
+                random(0, 255) +
+                ")";
+        });
+    }
+
+}
+
+function selectGridSize(selectGrid) {
+    selectGrid = prompt("Enter Grid Size");
+    if (selectGrid < 2 || selectGrid > 100) {
+        alert('Please select a number between 2 and 100');
+        selectGridSize();
+    }
+    else {
+        let rows = selectGrid;
+        let columns = selectGrid;
+        clearGrid();
+        createDivGrid(rows, columns);
+        
     }
 }
 
-createDivGrid(16, 16);
+function clearGrid() {
+    let divs = Array.from(divsContainer.childNodes);
+    divs.forEach((element) => {
+        divsContainer.removeChild(element);
+    });
+}
+
+selectGridButton.addEventListener('click', selectGridSize);
